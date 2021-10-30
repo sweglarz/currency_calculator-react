a304091ef6a36ae2c81979057d51d5a94999da38
Szymon Węglarz <simon.weglarz@gmail.com>
2021-10-30 20:08:20 +0200

    created currencies file

---
+++ src/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {   
+        summary: "EUR", 
+        name: "Euro", 
+        rate: 4.54,
+    },
+    {
+        summary: "USD", 
+        name: "Dolar Amerykański", 
+        rate: 3.79,
+    },
+    {
+        summary: "GBP", 
+        name: "Funt Szterling", 
+        rate: 5.22,
+    }
+]
