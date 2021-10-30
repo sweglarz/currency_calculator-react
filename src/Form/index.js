16ae27e1a19a94719415adef297223a5a454ef2f
Szymon Węglarz <simon.weglarz@gmail.com>
2021-10-30 20:10:33 +0200

    back to the orginal version + logic implemented

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,20 +1,63 @@
 import "./style.css"
-import Section from "../Section"
 import Button from "../Button"
 import Result from "../Result"
 import Introduction from "../Introduction"
+import { useState } from "react"
+import { currencies } from "../currencies"
 
-const Form = (props) => (
-    <form className="form">
-    <fieldset className="form__fieldset">
-        <Introduction/>
-        <Section/>
-        <Button/>
-        <Result/>
-    </fieldset>
-</form>
-)
-   
+const Form = ({ calculateResult, result }) => {
 
+    const [amount, setAmount] = useState("");
+    const [currency, setCurrency] = useState(currencies[0].summary)
 
-export default Form; 
+    const onFormSubmit = (event) => {
+        event.preventDefault()
+        calculateResult(amount, currency)
+    }
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <fieldset className="form__fieldset">
+                <Introduction />
+                <p className="form__paragraph">
+                    <label className="form__label">
+                        <span className="form__labelText">
+                            Podaj kwotę w PLN*:
+                        </span>
+                        <input
+                            value={amount}
+                            onChange={({ target }) => setAmount(target.value)}
+                            className="form__field"
+                            type="number"
+                            min="1"
+                            step="1"
+                            required
+                            placeholder="Podaj kwotę" />
+                    </label>
+                </p>
+                <p className="form__paragraph">
+                    <label className="form__label">
+                        <span className="form__labelText">
+                            Wymieniam na:
+                        </span>
+                        <select className="form__field"
+                            value={currency}
+                            onChange={({ target }) => setCurrency(target.value)}
+                        >
+                            {currencies.map((currency => (
+                                <option
+                                    key={currency.summary}
+                                    value={currency.summary}
+                                >
+                                    {currency.name}
+                                </option>
+                            )))};
+                        </select>
+                    </label>
+                </p>
+                <Button />
+                <Result result={result} />
+            </fieldset>
+        </form>
+    )
+}
+export default Form;
