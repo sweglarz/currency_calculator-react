022882a6bd89917ab464820e9743791c2838713b
Szymon Węglarz <simon.weglarz@gmail.com>
2021-10-30 20:08:04 +0200

    logic implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { Fragment } from "react";
-import Form from "./Form"
-import Header from "./Header"
+import Form from "./Form";
+import Header from "./Header";
+import {useState} from "react";
+import { currencies } from "./currencies";
 
 function App() {
-  
+  const [result, setResult] = useState();
+  const calculateResult = (amount, currency) => {
+    const findRate = currencies.find(({summary}) => summary === currency).rate;
+
+    setResult({
+      afterConversion: +amount / findRate,
+      currency,
+    });
+  };
+
   return (
     <>
-    <Header/>
-    <Form/>
+      <Header />
+      <Form calculateResult={calculateResult} result ={result}/>
     </>
-  )
-}
+  );
+};
 
 export default App;
