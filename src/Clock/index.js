8c0722e6b9943bc3d6976092878e746eca513e4a
Szymon Węglarz <simon.weglarz@gmail.com>
2021-11-21 16:04:25 +0100

    moved clock logic to a separate hook

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,30 +1,11 @@
-import { useEffect, useState } from "react";
 import { ClockElement } from "./styled";
+import { useCurrentDate } from "./useCurrentDate";
 
 const Clock = () => {
-    const [date, setDate] = useState(new Date());
-    
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date())
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId)
-        }
-    });
-
-    const clock = `${date.toLocaleTimeString()}`;
-    const actualDate = `${date.toLocaleDateString(undefined,
-        {
-            weekday: "long",
-            day: "numeric",
-            month: "long"
-        }
-    )}`;
+    const date = useCurrentDate();
 
     return (
-        <ClockElement>Dzisiaj jest {actualDate}, {clock}</ClockElement>
+        <ClockElement>Dzisiaj jest {date}</ClockElement>
     )
 };
 
---
+++ src/Clock/useCurrentDate.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export const useCurrentDate = () => {
+    const [date, setDate] = useState(new Date());
+    
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date())
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    });
+
+    const actualDate = `${date.toLocaleString(undefined,
+        {
+            weekday: "long",
+            hour: "2-digit",
+            minute:"2-digit",
+            second: "2-digit",
+            day: "numeric",
+            month: "long"
+        }
+    )}`;
+    return actualDate;
+}
