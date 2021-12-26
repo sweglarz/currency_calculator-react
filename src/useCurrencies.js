2717a4b29242331a42723282b0f2edb44b5ce6b3
Szymon Węglarz <simon.weglarz@gmail.com>
2021-12-26 12:27:32 +0100

    created a hook that retrieves data from api

---
+++ src/useCurrencies.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+export const useCurrencies = () => {
+    const [currencyData, setCurrencyData] = useState({ state: "loading" });
+
+    useEffect(() => {
+        const getRates = async () => {
+            try {
+                const myRates = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+                const { rates, date } = await myRates.data;
+
+                setCurrencyData({
+                    state: "loaded",
+                    rates,
+                    date
+                });
+            }
+            catch {
+                setCurrencyData({
+                    state: "error"
+                });
+            };
+        };
+        setTimeout(getRates, 1500);
+    }, []);
+ 
+    return currencyData;
+};
