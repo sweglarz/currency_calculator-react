5a6379eba82eda4beddf19d83ef4d336c3da3fdc
Szymon Węglarz <simon.weglarz@gmail.com>
2021-12-26 12:26:27 +0100

    logic moved to a form component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import Form from "./Form";
 import Header from "./Header";
-import {useState} from "react";
-import { currencies } from "./currencies";
 
 function App() {
-  const [result, setResult] = useState();
-  const calculateResult = (amount, currency) => {
-    const findRate = currencies.find(({summary}) => summary === currency).rate;
-
-    setResult({
-      afterConversion: +amount / findRate,
-      currency,
-    });
-  };
-
+ 
   return (
     <>
       <Header />
-      <Form calculateResult={calculateResult} result ={result}/>
+      <Form />
     </>
   );
 };
-
 export default App;
